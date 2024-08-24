ea46e058ab69c4e4685a792d69fae92aec763215
Rajinda Dissanayake <rajidissa@hotmail.com>
2024-08-24 02:45:13 +0100

    Initial commit

---
+++ src/main/java/org/phinxt/navigator/NavigatorApp.java
@@ -0,0 +1,11 @@
+package org.phinxt.navigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigatorApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NavigatorApp.class);
+    }
+}
