bc14b08eccd712018296b6226c28a8b6f1235766
Rajinda Dissanayake <rajidissa@hotmail.com>
2024-08-25 23:34:47 +0100

    Added swagger ui to the project

--- src/main/java/org/phinxt/navigator/config/ContentTypeFilter.java
+++ src/main/java/org/phinxt/navigator/config/ContentTypeFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.phinxt.navigator.utils.AppConstants;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,19 +13,38 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
+@Slf4j
 public class ContentTypeFilter extends OncePerRequestFilter {
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String contentType = request.getHeader(HttpHeaders.CONTENT_TYPE);
-        if (null == contentType || !contentType.contains(MediaType.APPLICATION_JSON_VALUE)) {
+        if (!getSkipUrlList().contains(request.getRequestURI()) &&
+                (null == contentType || !contentType.contains(MediaType.APPLICATION_JSON_VALUE))) {
+            log.info("Invalid request is received. Content-Type : {}", contentType);
             response.setStatus(HttpStatus.BAD_REQUEST.value());
             response.getWriter().write(AppConstants.INVALID_CONTENT_TYPE);
             return;
         }
         filterChain.doFilter(request, response);
+    }
+
+    private List<String> getSkipUrlList() {
+        return List.of(
+                "/swagger-ui/index.html",
+                "/swagger-ui/index.css",
+                "/swagger-ui/swagger-initializer.js",
+                "/swagger-ui/swagger-ui.css",
+                "/swagger-ui/swagger-ui-standalone-preset.js",
+                "/swagger-ui/swagger-ui-bundle.js",
+                "/swagger-ui/favicon-32x32.png",
+                "/swagger-ui/favicon-16x16.png",
+                "/v3/api-docs/swagger-config",
+                "/v3/api-docs"
+        );
     }
 }
